ce04383f370222b87c5b199ede256e92c5f95986
Shano1993 <stefan.hanotiau@gmail.com>
2021-11-26 11:42:45 +0100

    tab

--- exercice1.js
+++ exercice1.js
@@ -12,6 +12,49 @@ let articles = [
 
 // Votre code à partir d'ici :
 
+let listeCourse = document.getElementById('listeCourse');
+
+for (let i = 0; i < articles.length; i++) {
+    let elemLi = document.createElement('li');
+    elemLi.innerHTML = articles[i];
+    listeCourse.appendChild(elemLi);
+}
+
+
+let buttonDel = document.createElement('button');
+buttonDel.innerHTML = "Supprimer";
+listeCourse.prepend(buttonDel);
+
+let buttonAdd = document.createElement('button');
+buttonAdd.innerHTML = "Ajouter";
+listeCourse.prepend(buttonAdd);
+
+let champ = document.createElement('input');
+listeCourse.prepend(champ);
+
+buttonAdd.addEventListener("click", function () {
+
+})
+
+buttonDel.addEventListener("click", function () {
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
